c17ce6ac2ce0fb2b2b849fc4de9f790efbc09138
likvidov <likvidya@gmail.comm>
2023-06-25 15:22:17 +0300

    Добавлено изменение цвета квадрата, ширины и высоты круга, убрали кнопку,

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const square = document.getElementById('square');
+const eBtn = document.getElementById('e_btn');
+const range = document.getElementById('range');
+const circle = document.getElementById('circle');
 
+btn.addEventListener('click', function () {
+  square.style.backgroundColor = text.value;
+})
 
+eBtn.style.display = 'none';
 
-
-
+range.addEventListener('change', function () {
+  circle.style.height = range.value + '%';
+  circle.style.width = range.value + '%';
+})
